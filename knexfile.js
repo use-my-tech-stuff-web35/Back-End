6a6e8fd3f2fc22f3751a4141d7c654d51e5bcbc2
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-09-21 10:36:17 -0400

    ?

--- knexfile.js
+++ knexfile.js
@@ -15,5 +15,18 @@ module.exports = {
       directory: './data/seeds'
     },
   },
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {          
+      filename: './data/database.db3', // where to change the name of the database 
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  }
+}
 
-};
