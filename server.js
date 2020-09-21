5e080d0497a1f122f3f18288cb191de03b236c7c
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-09-21 11:19:52 -0400

    added routes for orders and items

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const helmet = require("helmet")
 const cors = require("cors")
 
 const authRouter = require("./auth/auth-router")
-const usersRouter = require("./router/orders-router")
+const ordersRouter = require("./router/orders-router")
+const itemsRouter = require("./router/items-router")
 const restricted = require("./auth/restricted-middleware.js")
 
 
@@ -19,7 +20,8 @@ server.use(
 )
 
 server.use("/api/auth", authRouter)
-server.use("/api/users", usersRouter)
+server.use("/api/items", itemsRouter)
+server.use("/api/orders", ordersRouter)
 
 server.get("/", (req, res) => {
     res.status(200).json({ Victor_Frankenstein: "It LIVEEEESSSSSSS" })
