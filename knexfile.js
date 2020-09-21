a837c3d0c3cbbb8323248e8a3a1a276ed9caf8b7
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-09-21 19:47:30 -0400

    last commit before bed

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,19 @@ module.exports = {
       directory: './data/seeds'
     },
   },
+  testing:{
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {          
+      filename: './data/database-testing.db3', // where to change the name of the database 
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
   production: {
     client: 'sqlite3',
     useNullAsDefault: true,
